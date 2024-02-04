491786fafeb91523ebc0b48330f4d0afafea6a2b
Andy <lad949739940@163.com>
2024-02-04 13:37:57 +0800

    🚀优化：添加了历史记录的功能

--- deploy-web/src/main/java/com/lad/DeploymentTool.java
+++ deploy-web/src/main/java/com/lad/DeploymentTool.java
@@ -3,19 +3,17 @@ package com.lad;
 import com.jcraft.jsch.*;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-import java.io.File;
+import java.io.*;
 
-import java.util.Vector;
+import java.util.*;
 
 
 /**
@@ -25,39 +23,43 @@ import java.util.Vector;
  */
 public class DeploymentTool extends Application {
 
+    // 历史记录文件名
+    private static final String FILE_NAME = "history.txt";
+    private static final String DIRECTORY_PATH = System.getProperty("user.dir");
+    private static final String FULL_PATH = DIRECTORY_PATH + File.separator + FILE_NAME;
+
+    // 五个变量对应的英文名
+    private static final String HOST = "host";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String LOCAL_PATH = "localPath";
+    private static final String SERVER_PATH = "serverPath";
+
+
     // 服务器信息
-    private final String USERNAME = "root";
-    private final String PASSWORD = "_WC5H3vnWQpgGMb";
-    private final String HOST = "150.158.22.85";
     private final int PORT = 22; // SSH 默认端口是 22
 
 
-    // 本地项目路径默认值
-    private String LOCAL_PATH_DEFAULT = "D:\\mine\\Project\\skyherb-qims\\skyhub-qims-front\\dist";
-    // 服务器项目路径默认值
-    private String SERVER_PATH_DEFAULT = "/var/www/river-skyherb/skyherb-qims-front/";
-
     @Override
     public void start(Stage primaryStage) {
+        Map<String, List<String>> historyMap = loadHistory();
+
         Label hostLabel = new Label("主机地址:");
-        TextField hostField = new TextField(HOST);
-        hostField.setPromptText("请输入主机地址");
+        ComboBox<String> hostField = createComboBox(HOST, "主机地址", historyMap);
 
         Label usernameLabel = new Label("用户名:");
-        TextField usernameField = new TextField(USERNAME);
-        usernameField.setPromptText("请输入用户名");
+        ComboBox<String> usernameField = createComboBox(USERNAME, "用户名", historyMap);
+
 
         Label passwordLabel = new Label("密码:");
-        TextField passwordField = new TextField(PASSWORD);
-        passwordField.setPromptText("请输入密码");
+        ComboBox<String> passwordField = createComboBox(PASSWORD, "密码", historyMap);
 
         Label localPathLabel = new Label("本地项目路径:");
-        TextField localPathField = new TextField(LOCAL_PATH_DEFAULT);
-        localPathField.setPromptText("请输入本地项目路径");
+        ComboBox<String> localPathField = createComboBox(LOCAL_PATH, "本地项目路径", historyMap);
+
 
         Label serverPathLabel = new Label("服务器项目路径:");
-        TextField serverPathField = new TextField(SERVER_PATH_DEFAULT);
-        serverPathField.setPromptText("请输入服务器项目路径");
+        ComboBox<String> serverPathField = createComboBox(SERVER_PATH, "服务器项目路径", historyMap);
 
 
         // 创建文本区域用于输出信息
@@ -67,22 +69,19 @@ public class DeploymentTool extends Application {
         // 创建按钮
         Button deployButton = new Button("一键部署");
 
+        // 五个字段的ComboBox List
+        List<ComboBox<String>> fields = Arrays.asList(hostField, usernameField, passwordField, localPathField, serverPathField);
+
         // 设置按钮的点击事件
         deployButton.setOnAction(e -> {
-            String localPath = localPathField.getText();
-            String serverPath = serverPathField.getText();
-            String username = usernameField.getText();
-            String password = passwordField.getText();
-            String host = hostField.getText();
 
-            new Thread(() -> buttonFunction(outputArea, localPath, serverPath, username, password, host)).start();
+            new Thread(() -> buttonFunction(outputArea, historyMap, fields)).start();
         });
 
         // 创建布局并添加组件
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
         layout.getChildren().addAll(hostLabel, hostField, usernameLabel, usernameField, passwordLabel, passwordField, localPathLabel, localPathField, serverPathLabel, serverPathField, outputArea, deployButton);
-
         // 设置场景
         Scene scene = new Scene(layout, 600, 500);
 
@@ -94,17 +93,55 @@ public class DeploymentTool extends Application {
 
 
     /**
+     * 创建一个hostField
+     *
+     * @param name       字段英文名
+     * @param cnName     字段中文名
+     * @param historyMap
+     * @return ComboBox<String>
+     */
+    private ComboBox<String> createComboBox(String name, String cnName, Map<String, List<String>> historyMap) {
+        ComboBox<String> field = new ComboBox<>();
+        field.setEditable(true);
+        field.setPromptText("请输入" + cnName);
+        // 获取对应历史记录
+        List<String> history = historyMap.getOrDefault(name, FXCollections.observableArrayList());
+        if (!history.isEmpty()) {
+            field.setValue(history.get(0));
+        }
+        field.getItems().addAll(history);
+        return field;
+    }
+
+    /**
      * 按钮点击事件
      *
      * @param outputArea 输出区域
-     * @param localPath  本地路径
-     * @param serverPath 服务器路径
-     * @param username   用户名
-     * @param password   密码
-     * @param host       主机地址
+     * @param historyMap 历史记录
+     * @param fields     五个字段的ComboBox
      */
-    private void buttonFunction(TextArea outputArea, String localPath, String serverPath, String username, String password, String host) {
+    private void buttonFunction(TextArea outputArea, Map<String, List<String>> historyMap, List<ComboBox<String>> fields) {
+
+        // 判断输入是否为空
+        for (ComboBox<String> field : fields) {
+            if (field.getValue() == null || field.getValue().isEmpty()) {
+                outputArea.appendText("请填写所有字段\n");
+                return;
+            }
+        }
+
+        // 获取输入的值
+        String host = fields.get(0).getValue();
+        String username = fields.get(1).getValue();
+        String password = fields.get(2).getValue();
+        String localPath = fields.get(3).getValue();
+        String serverPath = fields.get(4).getValue();
+
+
         outputArea.appendText("部署开始...\n");
+
+        // 更新历史记录
+        updateHistory(historyMap, fields);
 
 
         //  上传文件到服务器
@@ -132,6 +169,7 @@ public class DeploymentTool extends Application {
             outputArea.appendText("程序退出\n");
         }
 
+
     }
 
 
@@ -153,9 +191,9 @@ public class DeploymentTool extends Application {
     /**
      * 上传文件到服务器
      *
-     * @param localPath     本地dist路径
-     * @param serverPath    服务器路径
-     * @param outputArea    输出区域
+     * @param localPath  本地dist路径
+     * @param serverPath 服务器路径
+     * @param outputArea 输出区域
      */
     private void uploadDirectoryToServer(String localPath, String serverPath, TextArea outputArea, Session session) {
         try {
@@ -260,7 +298,117 @@ public class DeploymentTool extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+
+
+
+
+    /**
+     * 读取历史记录
+     *
+     * @return 历史记录
+     */
+    private static Map<String, List<String>> loadHistory() {
+        Map<String, List<String>> historyMap = new HashMap<>();
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return historyMap;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            List<String> currentList = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("#")) {
+                    currentList = historyMap.computeIfAbsent(line.substring(1), k -> new ArrayList<>());
+                } else if (currentList != null) {
+                    currentList.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return historyMap;
     }
+
+
+    /**
+     * 更新历史记录。
+     *
+     * @param historyMap 包含旧历史记录的Map。
+     * @param fields     五个字段的ComboBox 需要按照 HOST, USERNAME, PASSWORD, LOCAL_PATH, SERVER_PATH 的顺序
+     */
+    public static void updateHistory(Map<String, List<String>> historyMap, List<ComboBox<String>> fields) {
+        // 确保values列表中的顺序与historyMap中的键匹配
+        List<String> keys = Arrays.asList(HOST, USERNAME, PASSWORD, LOCAL_PATH, SERVER_PATH);
+
+
+        for (int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            String value = fields.get(i).getValue();
+
+            // 获取当前key的历史记录列表，如果不存在则创建新的列表
+            List<String> historyList = historyMap.computeIfAbsent(key, k -> new ArrayList<>());
+
+            // 如果列表已经包含当前值，则先移除
+            historyList.remove(value);
+
+            // 将当前值添加到列表的开头
+            historyList.add(0, value);
+        }
+        // 保存历史记录到文件
+        saveHistoryToFile(historyMap);
+        // 更新fields中的历史记录
+        for (int i = 0; i < keys.size(); i++) {
+            final ComboBox<String> field = fields.get(i);
+            List<String> history = historyMap.getOrDefault(keys.get(i), new ArrayList<>());
+
+            // 将更新操作放在JavaFX主线程上执行
+            Platform.runLater(() -> {
+                field.getItems().clear(); // 首先清除现有的项
+                if (!history.isEmpty()) {
+                    field.setValue(history.get(0)); // 设置默认值为最新的历史记录
+                }
+                field.getItems().addAll(history); // 添加所有历史记录
+            });
+        }
+    }
+
+
+    /**
+     * 将历史记录保存到文件中。
+     *
+     * @param history 包含历史记录的Map。
+     */
+    public static void saveHistoryToFile(Map<String, List<String>> history) {
+        try {
+            File file = new File(FULL_PATH);
+            if (!file.exists()) {
+                file.createNewFile(); // 如果文件不存在，则创建新文件
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, false))) {
+                    writer.write("#host"+ "\n"+ "#username"+ "\n"+ "#password"+ "\n"+ "#localPath"+ "\n"+ "#serverPath");
+                }
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, false))) {
+                for (Map.Entry<String, List<String>> entry : history.entrySet()) {
+                    writer.write("#" + entry.getKey());
+                    writer.newLine();
+                    for (String value : entry.getValue()) {
+                        writer.write(value);
+                        writer.newLine();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+
+    }
+
 }
---
+++ deploy-web/src/main/java/com/lad/HistoryTool.java
@@ -0,0 +1,8 @@
+package com.lad;
+
+/**
+ * @author Andy
+ * @date 2024-2-4 004 13:29
+ */
+public class HistoryTool {
+}
